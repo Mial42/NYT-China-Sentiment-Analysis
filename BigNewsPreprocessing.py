fab085089c8cc528277552083ddab4c812a2ca44
Nicholas Wilson <56737727+Mial42@users.noreply.github.com>
2023-04-28 20:10:19 -0400

    Update BigNewsPreprocessing.py

--- BigNewsPreprocessing.py
+++ BigNewsPreprocessing.py
@@ -3,6 +3,8 @@ import csv
 
 csv.field_size_limit(131072 * 2)
 
+acceptable_publications = set(['Washington Post', 'Vox', 'Reuters', 'The New York Times', 'CNBC', 'Business Insider', 'Axios', 'Economist', 'Fox News', 'The Hill'])
+acceptable_years = set(['2016','2017','2018','2019','2020'])
 # Open the input and output CSV files
 with open('big_news.csv', newline='', encoding='utf-8') as input_file, \
      open('china_articles.csv', 'w', newline='', encoding='utf-8') as output_file:
@@ -16,5 +18,9 @@ with open('big_news.csv', newline='', encoding='utf-8') as input_file, \
   
   # Loop through the input file and write matching rows to the output file
   for row in reader:
-    if 'China' in row[8] or 'Chinese' in row[8]:
-      writer.writerow(row)
+    #print(row[1] in acceptable_years)
+    #print(row[9])
+    if row[1] in acceptable_years and row[9] in acceptable_publications:
+
+      if 'China' in row[6] or 'Chinese' in row[6]:
+        writer.writerow(row)
